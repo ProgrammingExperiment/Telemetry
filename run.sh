d61067af34681c701e4953dd91cee25922cefbda
Luis Herrera <donluisherrera@example.com>
2017-05-29 19:28:54 -0700

    change run setting to restart

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-docker run -d --restart=always -p 80:80 -p 2003-2004:2003-2004 -p 2023-2024:2023-2024 -p 8125:8125/udp -p 8126
-:8126 graphite 
-docker run -i -d -p 3000:3000 grafana 
+docker restart graphite 
+docker restart grafana 
